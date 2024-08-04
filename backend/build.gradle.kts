c565fb84b30ef056c6ab9c95dcad3bea937856f1
pjy1368 <basejin0810@naver.com>
2024-08-04 13:12:58 +0900

    feat: QueryDSL 세팅

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,6 +30,10 @@ dependencies {
     // db
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.postgresql:postgresql:42.7.3")
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 
     // lombok
     compileOnly("org.projectlombok:lombok")
