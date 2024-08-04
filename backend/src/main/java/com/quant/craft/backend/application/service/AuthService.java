8c95c2d75923344dfb853ed6735430dcca9cf17f
pjy1368 <basejin0810@naver.com>
2024-08-04 18:49:14 +0900

    refactor: 임시 분기 처리

--- backend/src/main/java/com/quant/craft/backend/application/service/AuthService.java
+++ backend/src/main/java/com/quant/craft/backend/application/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.quant.craft.backend.application.service;
 
+import com.quant.craft.backend.domain.OAuthProvider;
 import com.quant.craft.backend.domain.User;
 import com.quant.craft.backend.infrastructure.client.KakaoOAuthClient;
 import lombok.RequiredArgsConstructor;
@@ -11,14 +12,21 @@ public class AuthService {
 
     private final KakaoOAuthClient kakaoClient;
 
-    public String getOAuthLoginUrl(String redirectUrl) {
-        return kakaoClient.getOAuthLoginUrl(redirectUrl);
+    public String getOAuthLoginUrl(String provider, String redirectUrl) {
+        return switch (OAuthProvider.from(provider)) {
+            case KAKAO -> {
+                yield kakaoClient.getOAuthLoginUrl(redirectUrl);
+            }
+        };
     }
 
-    public User kakaoOauthLogin(String authorizationCode) {
-        String accessToken = kakaoClient.requestAccessToken(authorizationCode);
-        User user = kakaoClient.requestUserInformation(accessToken);
-
-        return user;
+    public User oauthLogin(String provider, String authorizationCode) {
+        return switch (OAuthProvider.from(provider)) {
+            case KAKAO -> {
+                String accessToken = kakaoClient.requestAccessToken(authorizationCode);
+                User user = kakaoClient.requestUserInformation(accessToken);
+                yield user;
+            }
+        };
     }
 }
--- backend/src/main/java/com/quant/craft/backend/presentation/controller/AuthController.java
+++ backend/src/main/java/com/quant/craft/backend/presentation/controller/AuthController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -28,13 +29,13 @@ public class AuthController {
                 provider
         );
 
-        String url = authService.getOAuthLoginUrl(redirectUrl);
+        String url = authService.getOAuthLoginUrl(provider, redirectUrl);
         return new RedirectView(url);
     }
 
-    @GetMapping("/oauth/kakao")
-    public ResponseEntity<User> redirectKaKao(@RequestParam("code") String code) {
-        User user = authService.kakaoOauthLogin(code);
+    @GetMapping("/oauth/{provider}")
+    public ResponseEntity<User> login(@PathVariable("provider") String provider, @RequestParam("code") String code) {
+        User user = authService.oauthLogin(provider, code);
         return ResponseEntity.ok(user);
     }
 
