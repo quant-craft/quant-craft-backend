b3bf99b6c1ff72e54728c3339c1c239652f01a4c
Jayon <basejin0810@naver.com>
2024-10-26 22:27:50 +0900

    feat: TradingBot API 연동

--- src/main/java/com/quant/craft/backend/infrastructure/client/trade/TradeBotClient.java
+++ src/main/java/com/quant/craft/backend/infrastructure/client/trade/TradeBotClient.java
@@ -1,5 +1,8 @@
 package com.quant.craft.backend.infrastructure.client.trade;
 
+import com.quant.craft.backend.domain.trade.TradingBot;
+import com.quant.craft.backend.exception.BadRequestException;
+import com.quant.craft.backend.infrastructure.client.trade.dto.CreateTradingBotRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,4 +16,35 @@ public class TradeBotClient {
 
   @Value("${application.host.trade}")
   private String apiServerUrl;
+
+  public TradingBot create(CreateTradingBotRequest request) {
+    try {
+      String url = String.format("%s/trading-bots", apiServerUrl);
+      TradingBot response = client.post()
+              .uri(url)
+              .body(request)
+              .retrieve()
+              .body(TradingBot.class);
+
+        if (response == null) {
+            throw new BadRequestException("TradingBot cannot be null!");
+        }
+
+        return response;
+    } catch (Exception e) {
+      throw new BadRequestException("Create TradingBot Error. e: " + e);
+    }
+  }
+
+  public void delete(Long tradingBotId) {
+    try {
+      String url = String.format("%s/trading-bots/%d", apiServerUrl, tradingBotId);
+      client.delete()
+              .uri(url)
+              .retrieve()
+              .body(Void.class);
+    } catch (Exception e) {
+      throw new BadRequestException("Delete TradingBot Error. e: " + e);
+    }
+  }
 }
