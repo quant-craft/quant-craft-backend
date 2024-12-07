11ad8f658f1fe14b49fd6842d9c7cc006fdab42f
Jayon <basejin0810@naver.com>
2024-12-07 16:56:44 +0900

    refactor: consume 로직 수정

--- src/main/java/com/quant/craft/backend/presentation/controller/SseController.java
+++ src/main/java/com/quant/craft/backend/presentation/controller/SseController.java
@@ -8,6 +8,7 @@ import com.quant.craft.backend.exception.NotFoundException;
 import com.quant.craft.backend.infrastructure.repository.TradingBotRepository;
 import com.quant.craft.backend.presentation.argumentresolver.RequiredLogin;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,7 @@ public class SseController {
 
     private final TradingBotRepository tradingBotRepository;
 
-    @GetMapping("/demo-tradings/{botId}")
+    @GetMapping(path = "/demo-tradings/{botId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter demo(@RequiredLogin User user, @PathVariable Long botId) {
         TradingBot bot = tradingBotRepository.findById(botId)
                 .orElseThrow(() -> new NotFoundException("Trading bot not found"));
